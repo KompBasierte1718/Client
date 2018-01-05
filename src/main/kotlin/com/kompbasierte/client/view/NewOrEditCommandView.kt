2c3a9e3055820fb33235d9ac54e220451a1c7c29
Jan-Hendrik Sünderkamp <jhsuenderkamp@googlemail.com>
2018-01-05 16:13:05 +0100

    Erster Schritt: Tasten werden erkannt und ausgegeben

--- src/main/kotlin/com/kompbasierte/client/view/NewOrEditCommandView.kt
+++ src/main/kotlin/com/kompbasierte/client/view/NewOrEditCommandView.kt
@@ -1,27 +1,30 @@
 package com.kompbasierte.client.view
 
 import com.kompbasierte.client.model.Command
+import javafx.event.EventHandler
 import javafx.scene.control.*
+import javafx.scene.input.KeyEvent
 import tornadofx.*
+import java.awt.event.KeyAdapter
 
-class NewOrEditCommandView (val master:MainView): Fragment("New/Edit Command") {
+class NewOrEditCommandView(val master: MainView) : Fragment("New/Edit Command") {
     lateinit var nameText: TextField
     lateinit var firstComboBox: ComboBox<String>
     lateinit var secondComboBox: ComboBox<String>
-//    lateinit var thirdComboBox: ComboBox<String>
+    //    lateinit var thirdComboBox: ComboBox<String>
     override val root = vbox {
         hbox {
             label {
                 text = "Name: "
             }
-            nameText = textfield{}
+            nameText = textfield {}
         }
         spacing = 10.0
         tabpane {
             tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
 
             tab("Tastendruck") {
-                form {
+                /*form {
                     fieldset {
                         field("Shortcut") {
                             firstComboBox = combobox<String> {
@@ -36,6 +39,14 @@ class NewOrEditCommandView (val master:MainView): Fragment("New/Edit Command") {
                             }
                         }
                     }
+                }*/
+                button("Lauschen") {
+                    action {
+
+                        onKeyPressedProperty().set(EventHandler<KeyEvent>(fun(e: KeyEvent?) {
+                            println(e.toString())
+                        }))
+                    }
                 }
             }
             tab("Skript") {
@@ -43,11 +54,12 @@ class NewOrEditCommandView (val master:MainView): Fragment("New/Edit Command") {
                 tooltip = Tooltip("Später implementiert")
             }
         }
+
         buttonbar {
             button("Save") {
                 action {
                     master.saveCommandForApplication(Command(0, nameText.text, nameText.text,
-                            "" + firstComboBox.selectedItem + "+" + secondComboBox.selectedItem,true
+                            "" + firstComboBox.selectedItem + "+" + secondComboBox.selectedItem, true
                             /*TODO implement later + "+" + thirdComboBox.selectedItem*/))
                     close()
                 }
