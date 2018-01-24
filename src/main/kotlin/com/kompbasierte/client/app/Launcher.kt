db005d4ed306f4a6c4c4ac2e87563186eb9647fb
Jan-Hendrik Sünderkamp <jhsuenderkamp@googlemail.com>
2018-01-24 09:36:24 +0100

    Fix für Neuerstellung der MainView im onClose()

--- src/main/kotlin/com/kompbasierte/client/app/Launcher.kt
+++ src/main/kotlin/com/kompbasierte/client/app/Launcher.kt
@@ -3,13 +3,28 @@ package com.kompbasierte.client.app
 import javafx.application.Application
 import tornadofx.*
 import com.kompbasierte.client.view.MainView
+import javafx.stage.Stage
+import java.util.logging.Logger
 
-class Launcher: App(MainView::class, Styles::class){
-    override fun stop() {
-        super.stop()
-        find(MainView::class).onClose()
+class Launcher : App(MainView::class, Styles::class) {
 
+    lateinit var mainView:MainView
+
+    companion object {
+        private val LOG = Logger.getLogger(Control::class.java.name)
     }
+
+    override fun start(stage: Stage) {
+        super.start(stage)
+        mainView = stage.uiComponent()!!
+        LOG.info("Mainview beim Start gesetzt: "+ mainView.toString())
+    }
+
+    override fun stop() {
+        mainView.onClose()
+        super.stop()
+    }
+
 }
 
 fun main(args: Array<String>) {
